3170d3bb8dbdb04f20f6192f25a9fdf53d8d57dd
DarKov <dariusz_kowalczykk@wp.pl>
2021-11-25 21:42:50 +0100

    Second laboratory: LeapYear

---
+++ LeapYear/LeapYear/Calculations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapYear
+{
+    class Calculations
+    {
+        public static bool IsLeapyear(ushort year)
+        {    
+            
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+            {   
+                return true;
+            }
+            
+            else
+            {
+                return false;
+            }
+
+        }
+    }
+}
