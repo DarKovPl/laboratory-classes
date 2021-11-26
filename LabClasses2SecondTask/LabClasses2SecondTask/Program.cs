7cacc1a675f44b0ce60a2139dcaa8dc9c4451ae0
DarKov <dariusz_kowalczykk@wp.pl>
2021-11-26 20:44:49 +0100

    Second laboratory: enum type

---
+++ LabClasses2SecondTask/LabClasses2SecondTask/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabClasses2SecondTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enum type ConsoleColor is for changing colour change.");
+            Console.WriteLine("Text before colour changed.");
+            
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            
+            Console.WriteLine("Text after colour changed.");
+            
+            Console.ReadKey();
+        }
+    }
+}
+
+
+//static void Main(string[] args)
+//{
+//    Console.WriteLine("Typ wyliczeniowy ConsoleColor służy do zmiany koloru konsoli");
+//    //Zmiana koloru tła tekstu
+//    Console.BackgroundColor = ConsoleColor.Blue;
+//    //Kolor czcionki
+//    Console.ForegroundColor = ConsoleColor.Yellow;
+//    Console.WriteLine("Tekst po zmianie koloru");
+
+//    Console.ReadKey();
+//}
