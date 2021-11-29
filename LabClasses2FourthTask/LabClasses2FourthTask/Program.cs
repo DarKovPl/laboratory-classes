255722dee2c7b55e01f4bf73474caad030fe9b51
DarKov <dariusz_kowalczykk@wp.pl>
2021-11-29 20:20:48 +0100

    Second laboratory: LabClasses2FourthTask - The smallest and the biggest number

---
+++ LabClasses2FourthTask/LabClasses2FourthTask/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabClasses2FourthTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            Console.Write("Give me the first number: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Give me the second number: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Give me the third number: ");
+            c = Convert.ToDouble(Console.ReadLine());
+
+            if ((a <= b) && (a <= c))
+                Console.WriteLine("Number {0} is the smallest.", a);
+            else if ((b <= a) && (b <= c))
+                Console.WriteLine("Number {0} is the smallest.", b);
+            else
+                Console.WriteLine("Number {0} is the smallest.", c);
+
+            Console.WriteLine();
+
+            if ((a >= b) && (a >= c))
+                Console.WriteLine("Number {0} is the biggest.", a);
+            else if ((b >= a) && (b >= c))
+                Console.WriteLine("Number {0} is the biggest.", b);
+            else
+                Console.WriteLine("Number {0} is the biggest.", c);
+           
+            Console.ReadKey();
+
+        }
+    }
+}
--- LabClasses2ThirdTask/LabClasses2ThirdTask/Program.cs
+++ LabClasses2ThirdTask/LabClasses2ThirdTask/Program.cs
@@ -22,53 +22,18 @@ namespace LabClasses2ThirdTask
             Console.WriteLine("\tNotacja kompaktowa {0:g}\n", 23.45e-1); //Notacja kompaktowa 2.345
 
             Console.WriteLine("Bez grupowania cyfr: {0}", 3123456.56); //Bez grupowania cyfr: 3123456.56
-            Console.WriteLine("Z grupowaniem cyfr: {0:N}\n", 3123456.56);
+            Console.WriteLine("Z grupowaniem cyfr: {0:N}\n", 3123456.56); //Z grupowaniem cyfr: 3,123,456.56
 
-            Console.WriteLine("Liczba szesnastkowa: {0:x}\n", 23456);
+            Console.WriteLine("Liczba szesnastkowa: {0:x}\n", 23456); //Liczba szesnastkowa: 5ba0
 
-            Console.WriteLine("Procenty: {0:P}\n", 50);
+            Console.WriteLine("Procenty: {0:P}\n", 50); //Procenty: 5,000.00%
 
             Console.WriteLine("Wartość przybliżona: {0:f10}", 123456789.12345678); //Wartosc przyblizona: 123456789.1234570000
-            Console.WriteLine("Wartośc dokładna: {0:r}", 123456789.12345678); //Wartosc dokladna: 123456789.12345678
+            Console.WriteLine("Wartośc dokładna: {0:r}\n", 123456789.12345678); //Wartosc dokladna: 123456789.12345678
+
+            Console.WriteLine("Liczba PI w notacji naukowej z trzema miejscami po przecinku {0:e3}", Math.PI); //3.142e+000
 
             Console.ReadKey();
         }
     }
 }
-
-
-
-
-//static void Main(string[] args)
-//{
-
-//    Console.WriteLine("Cena: {0,20:c}.", 14); // wyrównanie do prawej, z podaniem waluty
-//    Console.WriteLine("Cena: {0,-20:C4}.", 14); // wyrównanie do lewej, dokładność 4 miejsca po przecinku
-
-//    Console.WriteLine();                        //odstęp
-//    Console.WriteLine("Liczby całkowite: {0:D20}", 14);
-
-//    Console.WriteLine("Liczby rzeczywiste:");
-//    Console.WriteLine("\tNotacja naukowa {0:e3}", 2.345);
-//    Console.WriteLine("\tNotacja naukowa {0:E1}", 2.345);
-//    Console.WriteLine("\tNotacja zwykła {0:f12}", 0.000000345);
-//    Console.WriteLine("\tNotacja kompaktowa {0:g}", 0.000000345);
-//    Console.WriteLine("\tNotacja kompaktowa {0:g}", 23.45e-1);
-
-//    Console.WriteLine();
-//    Console.WriteLine("Bez grupowania cyfr: {0}", 3123456.56);
-//    Console.WriteLine("Z grupowaniem cyfr: {0:N}", 3123456.56);
-
-//    Console.WriteLine();
-//    Console.WriteLine("Liczba szesnastkowa: {0:x}", 23456);
-//    Console.WriteLine("Liczba szesnastkowa: {0:X}", 23456);
-
-//    Console.WriteLine();
-//    Console.WriteLine("Procenty: {0:P}", 50);
-
-//    Console.WriteLine();
-//    Console.WriteLine("Wartość przybliżona: {0:f10}", 123456789.12345678);
-//    Console.WriteLine("Wartość dokładna: {0:r}", 123456789.12345678);
-
-//    Console.ReadKey();
-//}
