a442c5298dcb52b919847a6cc5051c60b2a5cd1d
DarKov <dariusz_kowalczykk@wp.pl>
2021-12-02 21:20:38 +0100

    Second laboratory: LabClasses2EighthTask - Exception handling - Done

--- LabClasses2EighthTask/LabClasses2EighthTask/Program.cs
+++ LabClasses2EighthTask/LabClasses2EighthTask/Program.cs
@@ -10,6 +10,53 @@ namespace LabClasses2EighthTask
     {
         static void Main(string[] args)
         {
+            //Division by zero. No compilation.
+            //int X;
+            //X = 10 / 0;
+
+            //Division by zero. Exception occurred. 
+            //int x, y, z;
+            //x = 10;
+            //y = 0;
+            //z = x / y;
+
+            int x, y, z;
+            x = 10;
+            y = 0;
+            try
+            {
+                z = x / y;
+            }
+            catch
+            {
+                Console.WriteLine("Don't try to divide by zero.");
+            }
+            finally
+            {
+                Console.WriteLine("Code from the block of finally.");
+            }
+
+            Console.WriteLine("Hit the key.");
+            Console.ReadKey();
+
+            double divisor, quotient;
+            try
+            {
+                Console.WriteLine("Give me a number: ");
+                divisor = Convert.ToDouble(Console.ReadLine());
+                if (divisor == 0)
+                    throw new DivideByZeroException("The divisor can't be zero.");
+                quotient = 10.2 / divisor; 
+                Console.WriteLine(divisor);
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine("Exception message: {0}", e.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
         }
     }
 }
