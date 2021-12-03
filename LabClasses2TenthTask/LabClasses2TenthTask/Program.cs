6f7df25e3236625c567c7cf490266daa8c57ca4d
DarKov <dariusz_kowalczykk@wp.pl>
2021-12-03 20:40:41 +0100

    Second laboratory: LabClasses2TenthTask - switch

---
+++ LabClasses2TenthTask/LabClasses2TenthTask/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabClasses2TenthTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int season;
+            Console.Write("Give me the season's number: ");
+            season = Convert.ToInt16(Console.ReadLine());
+            switch(season)
+            {
+                case 1: Console.WriteLine("Spring"); break;
+                case 2: Console.WriteLine("Summer"); break;
+                case 3: Console.WriteLine("Autumn"); break;
+                case 4: Console.WriteLine("Winter"); break;
+                default: Console.WriteLine("There is no season like this."); break;
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
