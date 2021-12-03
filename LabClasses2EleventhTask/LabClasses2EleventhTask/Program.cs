adc9ce08b4ab686ab2a3b3a12e0eda1e8ded7f3e
DarKov <dariusz_kowalczykk@wp.pl>
2021-12-03 21:12:19 +0100

    Second laboratory: LabClasses2EleventhTask - Center of the circle

---
+++ LabClasses2EleventhTask/LabClasses2EleventhTask/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabClasses2EleventhTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y;
+            double center_x = 0, center_y = 0, radiusOfCycle = 5;
+
+            Console.Write("Give me the coordinate x: \n");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Give me the coordinate y: \n");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            double distance = Math.Sqrt((x - center_x) * (x - center_x) + (y - center_y) * (y - center_y));
+
+            Console.Write("Distance between point ({0}, {1}) to the center of the circle ({2}, {3}) is {4, 4:G4}\n", x, y, center_x, center_y, distance);
+
+            Console.ReadKey();
+        
+        }
+    }
+}
