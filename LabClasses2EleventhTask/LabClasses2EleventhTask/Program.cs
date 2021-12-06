2be7cf87f65771a576a42fc066116c57dc11ed07
DarKov <dariusz_kowalczykk@wp.pl>
2021-12-06 20:41:28 +0100

    Second laboratory: LabClasses2EleventhTask - the middle of the circle

--- LabClasses2EleventhTask/LabClasses2EleventhTask/Program.cs
+++ LabClasses2EleventhTask/LabClasses2EleventhTask/Program.cs
@@ -23,6 +23,11 @@ namespace LabClasses2EleventhTask
 
             Console.Write("Distance between point ({0}, {1}) to the center of the circle ({2}, {3}) is {4, 4:G4}\n", x, y, center_x, center_y, distance);
 
+            if (distance <= radiusOfCycle)
+                Console.WriteLine("The point ({0}, {1}) is in the circle. The middle of the circle is in the point (0, 0), and the radius is 5.", x, y);
+            else
+                Console.WriteLine("The point ({0}, {1}) is not in the circle.", x, y);
+            
             Console.ReadKey();
         
         }
