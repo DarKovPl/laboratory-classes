846976703cb26501bb64557c423137f2f080263a
DarKov <dariusz_kowalczykk@wp.pl>
2021-12-06 21:44:33 +0100

    Third laboratory: LabClasses3LoopsBeginFirstTask

---
+++ LabClasses3LoopsBeginFirstTask/LabClasses3LoopsBeginFirstTask/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabClasses3LoopsBeginFirstTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1, s = 0;
+
+            while (i <= 10)
+            {
+                s += i;
+                i += 1;
+                Console.WriteLine("s: {0}\ni: {0}\n", s, i);
+
+            }
+            Console.Write(s);
+            Console.ReadKey();
+        }
+    }
+}
