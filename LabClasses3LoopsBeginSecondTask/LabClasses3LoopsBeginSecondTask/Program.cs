1c0bb61a1a27952792e4bee129475af16f42e9f8
DarKov <dariusz_kowalczykk@wp.pl>
2021-12-07 20:44:01 +0100

    Third laboratory: LabClasses3LoopsBeginSecondTask - while to for

---
+++ LabClasses3LoopsBeginSecondTask/LabClasses3LoopsBeginSecondTask/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabClasses3LoopsBeginSecondTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1, s = 0;
+            for (int i = 1; i <= 10; i++)
+            {
+                s += n;
+                n += 1;
+                Console.WriteLine("s: {0}\ni: {1}\nn: {2}\n", s, i, n);
+
+            }
+            Console.Write(s);
+            Console.ReadKey();
+
+
+        }
+    }
+}
