2a6878237f42e678e3251f449280f0d45e3d9253
DarKov <dariusz_kowalczykk@wp.pl>
2021-12-10 21:12:14 +0100

    Third laboratory: LabClasses3LoopsBeginEighthTask - division by seven

---
+++ LabClassesLoopsBeginEighthTask/LabClassesLoopsBeginEighthTask/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabClassesLoopsBeginEighthTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me the range of division: ");
+            byte howMeny = Convert.ToByte(Console.ReadLine()), stop = 0;
+
+            List<string> divisionList = new List<string>();
+
+
+            for (int i = 0; ; i++)
+            {
+
+                if (i == 0)
+                {
+                    divisionList.Add(i.ToString());
+                }
+            
+                else if (i % 7 == 0)
+                {
+                    stop += 1;
+                    
+                    if (stop >= howMeny)
+                    {
+                        break;
+                    }
+                    
+                    divisionList.Add(i.ToString());
+                }
+
+            }
+            string[] divisionArray = divisionList.ToArray();
+            Console.WriteLine("All numbers can be divided by seven: " + String.Join(", ", divisionArray));
+            Console.ReadKey();
+        }
+    }
+}
