fba9dabe1890a5578364393fa2dd9f4d9d09234b
DarKov <dariusz_kowalczykk@wp.pl>
2021-12-15 20:39:43 +0100

    Fourth laboratory: LabClasses4LoopsBeginThirteenthTask - linear function

---
+++ LabClasses4LoopsBeginThirteenthTask/LabClasses4LoopsBeginThirteenthTask/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabClasses4LoopsBeginThirteenthTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = -10;
+            
+            Console.WriteLine("Give me a:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Give me b:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("  x | y");
+            
+            while (x <= 10)
+            {
+                int y = a * x + b;
+                Console.WriteLine("{0,3} | {1,2}", x, y);
+
+                x += 1; 
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
