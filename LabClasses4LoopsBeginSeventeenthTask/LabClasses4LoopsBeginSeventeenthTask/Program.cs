bcf549a33864a68bc1c341b05d5d4a3f0a796169
DarKov <dariusz_kowalczykk@wp.pl>
2021-12-16 20:06:22 +0100

    Fourth laboratory: LabClasses4LoopsBeginSeventeenthTask - while to for block diagram

---
+++ LabClasses4LoopsBeginSeventeenthTask/LabClasses4LoopsBeginSeventeenthTask/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabClasses4LoopsBeginSeventeenthTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me the constant: ");
+            int constant = Convert.ToInt32(Console.ReadLine()), i = 1, j = 2, s = 0;
+            for(; i <= constant; i++)
+            {
+                s += j;
+                j += 2;
+            }
+            Console.WriteLine(s);
+            Console.ReadKey();
+        
+        }
+    }
+}
--- LabClasses4LoopsBeginSixteenthTask/LabClasses4LoopsBeginSixteenthTask/Program.cs
+++ LabClasses4LoopsBeginSixteenthTask/LabClasses4LoopsBeginSixteenthTask/Program.cs
@@ -10,6 +10,9 @@ namespace LabClasses4LoopsBeginSixteenthTask
     {
         static void Main(string[] args)
         {
+            // This program is able to count the sum of two variables (s + j) and additionally, one of them "j" is increased by 2 in every loop.
+            // At the end, the variable "s" is printed on the screen. Variable "i" is the counter in this application.
+
             int i = 1, j = 2, s = 0;
 
             while(i <= 10)
@@ -17,16 +20,6 @@ namespace LabClasses4LoopsBeginSixteenthTask
                 s += j;
                 j += 2;
                 i += 1;
-
-            }
-            Console.WriteLine(s);
-            Console.ReadKey();
-
-            while( i <= 10)
-            {
-                s = s + j;
-                j = j + 2;
-                i = i + 1;
 
             }
             Console.WriteLine(s);
