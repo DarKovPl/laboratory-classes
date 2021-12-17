a8ca7a778ae6bc207e85431e359d81bfb7a435b5
DarKov <dariusz_kowalczykk@wp.pl>
2021-12-17 11:07:58 +0100

    Fourth laboratory: LabClasses4LoopsBeginTwenty-firstTask - NWD: (4, 6), (21, 34) - block diagram

---
+++ LabClasses4LoopsBeginTwenty-firstTask/LabClasses4LoopsBeginTwenty-firstTask/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabClasses4LoopsBeginTwenty_firstTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me the a number: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Give me the b number: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("----------");
+
+            while (a != b)
+            {
+                Console.WriteLine("{0}", a);
+
+                if (a > b)
+                {
+                    a -= b;
+                }
+
+                else
+                {
+                    b = b - a;
+                }
+            }
+            // Console.WriteLine("NWD: {0}", a); In the task there is no mention of writing NWD on the console.
+            Console.ReadKey();
+        }   
+    }
+}
