3f414e459fbcee944933e51ca085bfdcfb0720fa
DarKov <dariusz_kowalczykk@wp.pl>
2022-01-10 21:15:02 +0100

    SmallSideTasks - rectangle created with stars in for loop

---
+++ SmallSideTasks/SmallSideTasks/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace SmallSideTasks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give the number grater than two.");
+            int a = Convert.ToInt16(Console.ReadLine());
+            
+
+            for(var i = 1; i <= a; ++i)
+                if ((i == 1) || (i == a))
+                {
+                    string stars = new string('*', a);
+                    Console.WriteLine(stars);
+                }
+                else
+                {
+                    for (int m = 1; m <= a; ++m)
+                    {
+                        if (m == 1 || m == a)
+                        {
+                            string star = "*";
+                            Console.Write(star);
+                        }
+                        else
+                        {
+                            string empty = " ";
+                            Console.Write(empty);
+
+                        }
+                    }   Console.WriteLine();
+                    
+                }
+            Console.ReadKey();
+        }
+    }
+}
