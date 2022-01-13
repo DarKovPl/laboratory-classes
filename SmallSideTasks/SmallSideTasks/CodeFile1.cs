c5d32130f87ac836f74f8835cd030c49a3ae40a3
DarKov <dariusz_kowalczykk@wp.pl>
2022-01-13 20:12:27 +0100

    SmallSideTasks - task 5

---
+++ SmallSideTasks/SmallSideTasks/CodeFile1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+
+namespace SmallSideTasks
+{
+    class Program1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many natural numbers divided by 11 and NOT divided by 3 do you want to see?");
+            int howMany = Convert.ToInt16(Console.ReadLine());
+            int counter = 0;
+
+            while (howMany > 0)
+            {
+                
+
+                int naturalNumDivided = ((counter % 11 == 0) && (counter % 3 != 0)) ? counter : -1;
+                counter++;
+
+                if (naturalNumDivided != -1)
+                {
+                    Console.WriteLine(naturalNumDivided);
+                    howMany--;
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
