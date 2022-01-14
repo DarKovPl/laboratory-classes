e23ece2bbb3475dc7344bb7b7fe7c735e05de6ee
DarKov <dariusz_kowalczykk@wp.pl>
2022-01-14 20:24:22 +0100

    SmallSideTasks - task 2 - mathematical sequence n>0

--- SmallSideTasks/SmallSideTasks/CodeFile4.cs
+++ SmallSideTasks/SmallSideTasks/CodeFile4.cs
@@ -9,7 +9,11 @@ namespace SmallSideTasks
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Give me number grater than zero.");
+            uint n = Convert.ToUInt16(Console.ReadLine());
+            double result = (1 + Math.Pow(n, 2)) / 2; 
+
+            Console.WriteLine(result);
             Console.ReadKey();
 
         }
