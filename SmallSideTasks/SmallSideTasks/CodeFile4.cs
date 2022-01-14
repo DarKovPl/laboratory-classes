b15db57075f3efb78e63b364587a8d7ee7581fb5
DarKov <dariusz_kowalczykk@wp.pl>
2022-01-14 20:12:42 +0100

    SmallSideTasks - a and b number negative, positive or equal zero

---
+++ SmallSideTasks/SmallSideTasks/CodeFile4.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+
+namespace SmallSideTasks
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.ReadKey();
+
+        }
+
+    }
+}
