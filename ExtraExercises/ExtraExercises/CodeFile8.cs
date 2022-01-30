6ef6ed9e4a3aca94009b582c8d71490661c22245
DarKov <dariusz_kowalczykk@wp.pl>
2022-01-30 17:58:28 +0100

    Extra exercises

--- ExtraExercises/ExtraExercises/CodeFile8.cs
+++ ExtraExercises/ExtraExercises/CodeFile8.cs
@@ -34,26 +34,7 @@ namespace ExtraExercises
 
             //46
 
-            Console.Write("Enter number: ");
-
-            int num = Convert.ToInt32(Console.In.ReadLine());
-
-            for (int i = -1; i < num; i++)
-            {
-
-                for (int j = num - i; j > 0; j--)
-                
-                    Console.Write(" ");
-
-                for (int j = 0; j <= i; j++)
-
-                    Console.Write("*" + "*");
-
-                Console.WriteLine();
-
-
-
-            }
+            
         }
     }
 }
